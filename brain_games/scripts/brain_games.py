8b2b9468dac6f4076ac340d1aa9ebaa0468e7932
Mrac044 <doctor.06@bk.ru>
2026-01-23 19:09:24 +0700

    fix brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,42 +1,12 @@
-from cli import welcome_user
-from games.brain_calc import main as calc_main
-from games.brain_even import main as even_main
-from games.brain_gcd import main as gcd_main
-from games.brain_progression import main as prog_main
-from engine import main as run
+from .cli import welcome_user
+
 
 def greet():
     print('Welcome to my application!')
 
-def brain_games(name): 
-    choice = -1
-
-    while choice != 0:
-        choice = int(input("""
-Выберите игру:
-1 - калькулятор
-2 - четность числа
-3 - нод
-4 - progression
-
-0 - выход
-Ваш выбор: 
-"""))
-
-        match choice:
-            case 1:
-                run(calc_main, name)
-            case 2:
-                run(even_main, name)
-            case 3:
-                run(gcd_main, name)
-            case 4:
-                run(prog_main, name)
-
 def main():
     greet()
-    name = welcome_user()
-    brain_games(name)
+    welcome_user()
 
 if __name__ == "__main__":
     main()
